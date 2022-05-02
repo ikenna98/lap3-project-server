dc9b4543ef70a2f63bb2532e3121631c78da31ac
Ikenna <ikennaagada@gmail.com>
2022-05-02 21:59:17 +0100

    updated db config

--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,15 @@
-const { Pool } = require("pg");
-const pool = new Pool(
-    {
-    user: 'lap3',
-    host: 'localhost',
-    database: 'lap3project',
-    password: 'password',
-    port: 5432,
+const { Pool } = require('pg');
+
+let pool;
+if (process.env.NODE_ENV === 'production') {
+      pool = new Pool({
+  	    connectionString: process.env.DATABASE_URL,
+  	    ssl: {
+  		    rejectUnauthorized: false,
+  	    },
+  });
+} else {
+    pool = new Pool();
 }
-);
+
 module.exports = pool;
